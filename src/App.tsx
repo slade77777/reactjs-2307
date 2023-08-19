62b191859a225a0b1d8757b281660022a68eab25
cuongdang <cuongdx@bfast.dev>
2023-08-19 21:11:02 +0700

    comit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,55 @@
 import './App.css'
-import {useState} from "react";
+import {useRef, useState} from "react";
 
-function App() {
-  const [name, setName] = useState('')
-  const [isShowResult, setShowResult] = useState(false);
+type UserType = {
+  firstName: string
+  lastName: string
+}
+
+const Form = ({addUser}: {addUser: (val: UserType) => void}) => {
+  const [firstName, setFirstName] = useState('');
+  const lastNameRef  = useRef(null);
 
   return (
     <div>
-      <label>Name: </label>
-      <input type='text' onChange={e => setName(e.target.value)} />
-      {name.length === 0 && <p>Please input name</p>}
-      {name.length > 10 && <p>Name is too long</p>}
-      <button onClick={() => setShowResult(true)}>Send</button>
-      {isShowResult && <p>Name inputted: {name}</p>}
+      <div>
+      <label>First Name: </label>
+      <input type='text' value={firstName} onChange={e => setFirstName(e.target.value)} />
+      </div>
+      <div>
+      <label>Last Name: </label>
+      <input type='text' ref={lastNameRef} />
+      </div>
+      <button onClick={() => {
+        addUser({firstName, lastName: lastNameRef.current?.value});
+        setFirstName('');
+        lastNameRef.current.value = '';
+      }}>Send</button>
+    </div>
+    )
+}
+
+const Result = ({userList}: {userList: Array<UserType>}) => {
+  return <div>
+    {
+      userList.map((user, index) => <div>
+        <p>Firstname: {user.firstName} - LastName: {user.lastName}</p>
+      </div>)
+    }
+  </div>
+}
+
+function App() {
+  const [userList, setUserList] = useState<Array<UserType>>([])
+
+  function addUser(user: UserType) {
+    setUserList([...userList, user])
+  }
+
+  return (
+    <div>
+      <Form addUser={addUser} />
+      <Result userList={userList} />
     </div>
   )
 }
